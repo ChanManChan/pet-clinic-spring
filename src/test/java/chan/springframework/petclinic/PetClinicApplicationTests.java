30563a761cd74e803f1d727835d0d5ee16a37028
NandaGopal <ngopal253@gmail.com>
2020-11-28 11:40:21 +0530

    POJO done

---
+++ src/test/java/chan/springframework/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package chan.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
