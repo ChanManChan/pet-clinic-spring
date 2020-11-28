7c82581d8da8227b94a2497e68ff00e8476d0a2c
NandaGopal <ngopal253@gmail.com>
2020-11-28 13:07:23 +0530

    multi-module

--- src/main/java/chan/springframework/petclinic/model/Owner.java
+++
@@ -1,4 +0,0 @@
-package chan.springframework.petclinic.model;
-
-public class Owner extends Person {
-}
--- src/main/java/chan/springframework/petclinic/model/Vet.java
+++
@@ -1,4 +0,0 @@
-package chan.springframework.petclinic.model;
-
-public class Vet extends Person {
-}
