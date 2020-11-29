141395b23b915f91c48945d658f733320854241f
NandaGopal <ngopal253@gmail.com>
2020-11-29 05:33:40 +0530

    adding base entity

---
+++ pet-clinic-data/src/main/java/chan/springframework/petclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package chan.springframework.petclinic.services;
+
+import chan.springframework.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
